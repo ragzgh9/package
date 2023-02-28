f08ddc3b3a1cd779d7594337a057fc8f032beff1
ragzgh9 <122343476+ragzgh9@users.noreply.github.com>
2023-02-28 15:06:41 +0700

    Add files via upload

---
+++ Paket.py
@@ -0,0 +1,12 @@
+from Package.modul1 import *
+from Package.modul2 import *
+from Package.HitungNilai import hitung_nilai,kali_nilai2
+def main():
+    f1()
+    f2()
+
+    f3()
+    f4()
+    print("Nilai Akhir : ",hitung_nilai(58,86))
+    print("Nilai bilek: ",kali_nilai2(67,15))
+main()
